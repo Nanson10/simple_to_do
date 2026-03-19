2e41af0ce17436f11ed53320919a353b9a77af5d
Nanson Chen <123785854+Nanson10@users.noreply.github.com>
2026-03-18 23:34:00 -0400

    Improved readability of the run script

--- run.sh
+++ run.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-cd $(dirname "$0")
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
 echo "Building for testing (1/3)"
 cargo build
-if [ "$?" -ne 0 ]; then
-    echo "Build failed"
-    return 1
-fi
+
 clear
 echo "Running Tests (2/3)"
 cargo test
-if [ "$?" -ne 0 ]; then
-    echo "Tests failed"
-    return 1
-fi
+
 clear
-echo "Setting up Application (3/3)"
-cargo build --release &&
+echo "Running Application (3/3)"
+./target/debug/simple_to_do
 clear
-./target/release/simple_to_do
