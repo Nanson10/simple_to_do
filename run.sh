2e04a1a68ab452d51ba266590b3a8ba0eae3b7dd
Nanson Chen <123785854+Nanson10@users.noreply.github.com>
2026-03-18 20:28:24 -0400

    Added echos for run workflow

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 cd $(dirname "$0")
+echo "Building for testing (1/3)"
 cargo build
 if [ "$?" -ne 0 ]; then
     echo "Build failed"
     return 1
 fi
 clear
+echo "Running Tests (2/3)"
 cargo test
 if [ "$?" -ne 0 ]; then
     echo "Tests failed"
     return 1
 fi
 clear
+echo "Setting up Application (3/3)"
 cargo build --release &&
 clear
 ./target/release/simple_to_do
